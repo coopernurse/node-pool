bad141b84c8cecf4115f3fedcde6fcf2c8257175
James Edward Butler <james.butler@sandfox.co.uk>
2017-10-15 12:27:43 +0100

    apply new eslint rules formatting changes

--- test/generic-pool-acquiretimeout-test.js
+++ test/generic-pool-acquiretimeout-test.js
@@ -1,72 +1,77 @@
-'use strict'
+"use strict";
 
-const tap = require('tap')
-const createPool = require('../').createPool
+const tap = require("tap");
+const createPool = require("../").createPool;
 
-tap.test('acquireTimeout handles timed out acquire calls', function (t) {
+tap.test("acquireTimeout handles timed out acquire calls", function(t) {
   const factory = {
-    create: function () {
-      return new Promise(function (resolve) {
-        setTimeout(function () {
-          resolve({})
-        }, 100)
-      })
+    create: function() {
+      return new Promise(function(resolve) {
+        setTimeout(function() {
+          resolve({});
+        }, 100);
+      });
     },
-    destroy: function () { return Promise.resolve() }
-  }
+    destroy: function() {
+      return Promise.resolve();
+    }
+  };
   const config = {
     acquireTimeoutMillis: 20,
     idleTimeoutMillis: 150,
     log: false
-  }
+  };
 
-  const pool = createPool(factory, config)
+  const pool = createPool(factory, config);
 
-  pool.acquire()
-  .then(function (resource) {
-    t.fail('wooops')
-  })
-  .catch(function (err) {
-    t.match(err, /ResourceRequest timed out/)
-    return pool.drain()
-  })
-  .then(function () {
-    return pool.clear()
-  })
-  .then(function () {
-  })
-  .then(t.end)
-  .catch(t.error)
-})
+  pool
+    .acquire()
+    .then(function(resource) {
+      t.fail("wooops");
+    })
+    .catch(function(err) {
+      t.match(err, /ResourceRequest timed out/);
+      return pool.drain();
+    })
+    .then(function() {
+      return pool.clear();
+    })
+    .then(function() {})
+    .then(t.end)
+    .catch(t.error);
+});
 
-tap.test('acquireTimeout handles non timed out acquire calls', function (t) {
-  const myResource = {}
+tap.test("acquireTimeout handles non timed out acquire calls", function(t) {
+  const myResource = {};
   const factory = {
-    create: function () {
-      return new Promise(function (resolve) {
-        setTimeout(function () {
-          resolve(myResource)
-        }, 10)
-      })
+    create: function() {
+      return new Promise(function(resolve) {
+        setTimeout(function() {
+          resolve(myResource);
+        }, 10);
+      });
     },
-    destroy: function () { return Promise.resolve() }
-  }
+    destroy: function() {
+      return Promise.resolve();
+    }
+  };
 
   const config = {
     acquireTimeoutMillis: 400
-  }
+  };
 
-  const pool = createPool(factory, config)
+  const pool = createPool(factory, config);
 
-  pool.acquire()
-  .then(function (resource) {
-    t.equal(resource, myResource)
-    pool.release(resource)
-    return pool.drain()
-  })
-  .then(function () {
-    return pool.clear()
-  })
-  .then(t.end)
-  .catch(t.error)
-})
+  pool
+    .acquire()
+    .then(function(resource) {
+      t.equal(resource, myResource);
+      pool.release(resource);
+      return pool.drain();
+    })
+    .then(function() {
+      return pool.clear();
+    })
+    .then(t.end)
+    .catch(t.error);
+});
--- test/resource-request-test.js
+++ test/resource-request-test.js
@@ -1,60 +1,60 @@
-var tap = require('tap')
-var ResourceRequest = require('../lib/ResourceRequest')
+var tap = require("tap");
+var ResourceRequest = require("../lib/ResourceRequest");
 
-tap.test('can be created', function (t) {
-  var create = function () {
-    var request = new ResourceRequest(undefined, Promise) // eslint-disable-line no-unused-vars
-  }
-  t.doesNotThrow(create)
-  t.end()
-})
+tap.test("can be created", function(t) {
+  var create = function() {
+    var request = new ResourceRequest(undefined, Promise); // eslint-disable-line no-unused-vars
+  };
+  t.doesNotThrow(create);
+  t.end();
+});
 
-tap.test('times out when created with a ttl', function (t) {
-  var reject = function (err) {
-    t.match(err, /ResourceRequest timed out/)
-    t.end()
-  }
-  var resolve = function (r) {
-    t.fail('should not resolve')
-  }
-  var request = new ResourceRequest(10, Promise) // eslint-disable-line no-unused-vars
+tap.test("times out when created with a ttl", function(t) {
+  var reject = function(err) {
+    t.match(err, /ResourceRequest timed out/);
+    t.end();
+  };
+  var resolve = function(r) {
+    t.fail("should not resolve");
+  };
+  var request = new ResourceRequest(10, Promise); // eslint-disable-line no-unused-vars
 
-  request.promise.then(resolve, reject)
-})
+  request.promise.then(resolve, reject);
+});
 
-tap.test('calls resolve when resolved', function (t) {
-  var resource = {}
-  var resolve = function (r) {
-    t.equal(r, resource)
-    t.end()
-  }
-  var reject = function (err) {
-    t.error(err)
-  }
-  var request = new ResourceRequest(undefined, Promise)
-  request.promise.then(resolve, reject)
-  request.resolve(resource)
-})
+tap.test("calls resolve when resolved", function(t) {
+  var resource = {};
+  var resolve = function(r) {
+    t.equal(r, resource);
+    t.end();
+  };
+  var reject = function(err) {
+    t.error(err);
+  };
+  var request = new ResourceRequest(undefined, Promise);
+  request.promise.then(resolve, reject);
+  request.resolve(resource);
+});
 
-tap.test('removeTimeout removes the timeout', function (t) {
-  var reject = function (err) {
-    t.error(err)
-  }
-  var request = new ResourceRequest(10, Promise)
-  request.promise.then(undefined, reject)
-  request.removeTimeout()
-  setTimeout(function () {
-    t.end()
-  }, 20)
-})
+tap.test("removeTimeout removes the timeout", function(t) {
+  var reject = function(err) {
+    t.error(err);
+  };
+  var request = new ResourceRequest(10, Promise);
+  request.promise.then(undefined, reject);
+  request.removeTimeout();
+  setTimeout(function() {
+    t.end();
+  }, 20);
+});
 
-tap.test('does nothing if resolved more than once', function (t) {
-  var request = new ResourceRequest(undefined, Promise)
-  t.doesNotThrow(function () {
-    request.resolve({})
-  })
-  t.doesNotThrow(function () {
-    request.resolve({})
-  })
-  t.end()
-})
+tap.test("does nothing if resolved more than once", function(t) {
+  var request = new ResourceRequest(undefined, Promise);
+  t.doesNotThrow(function() {
+    request.resolve({});
+  });
+  t.doesNotThrow(function() {
+    request.resolve({});
+  });
+  t.end();
+});
