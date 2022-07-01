0a5ef1dd9124ea7d56626c7cc295e181c15ad4e2
Teow Hua Jun <huajun@Teows-MacBook-Pro.local>
2022-07-01 15:55:44 +0800

    fix: add ready function to index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -7,44 +7,45 @@
 import { EventEmitter } from "events";
 
 export class Pool<T> extends EventEmitter {
-    spareResourceCapacity: number;
-    size: number;
-    available: number;
-    borrowed: number;
-    pending: number;
-    max: number;
-    min: number;
+  spareResourceCapacity: number;
+  size: number;
+  available: number;
+  borrowed: number;
+  pending: number;
+  max: number;
+  min: number;
 
-    start(): void;
-    acquire(priority?: number): Promise<T>;
-    release(resource: T): Promise<void>;
-    destroy(resource: T): Promise<void>;
-    drain(): Promise<void>;
-    clear(): Promise<void>;
-    use<U>(cb: (resource: T) => U | Promise<U>): Promise<U>;
-    isBorrowedResource(resource: T): boolean;
+  start(): void;
+  acquire(priority?: number): Promise<T>;
+  release(resource: T): Promise<void>;
+  destroy(resource: T): Promise<void>;
+  drain(): Promise<void>;
+  clear(): Promise<void>;
+  use<U>(cb: (resource: T) => U | Promise<U>): Promise<U>;
+  isBorrowedResource(resource: T): boolean;
+  ready(): Promise<void>;
 }
 
 export interface Factory<T> {
-    create(): Promise<T>;
-    destroy(client: T): Promise<void>;
-    validate?(client: T): Promise<boolean>;
+  create(): Promise<T>;
+  destroy(client: T): Promise<void>;
+  validate?(client: T): Promise<boolean>;
 }
 
 export interface Options {
-    max?: number;
-    min?: number;
-    maxWaitingClients?: number;
-    testOnBorrow?: boolean;
-    testOnReturn?: boolean;
-    acquireTimeoutMillis?: number;
-    fifo?: boolean;
-    priorityRange?: number;
-    autostart?: boolean;
-    evictionRunIntervalMillis?: number;
-    numTestsPerEvictionRun?: number;
-    softIdleTimeoutMillis?: number;
-    idleTimeoutMillis?: number;
+  max?: number;
+  min?: number;
+  maxWaitingClients?: number;
+  testOnBorrow?: boolean;
+  testOnReturn?: boolean;
+  acquireTimeoutMillis?: number;
+  fifo?: boolean;
+  priorityRange?: number;
+  autostart?: boolean;
+  evictionRunIntervalMillis?: number;
+  numTestsPerEvictionRun?: number;
+  softIdleTimeoutMillis?: number;
+  idleTimeoutMillis?: number;
 }
 
 export function createPool<T>(factory: Factory<T>, opts?: Options): Pool<T>;
